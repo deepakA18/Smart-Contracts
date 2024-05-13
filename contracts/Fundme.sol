5fed0a74b26ffadd8f4bb2472d1e284409494f0f
deepakagashe18 <agashedeepak55@gmail.com>
2024-05-13 18:24:55 +0530

    special functions

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -6,13 +6,21 @@ import {PriceConverter} from "./PriceConverter.sol";
 
 pragma solidity ^0.8.24;
 
+error NotOwner();
+
 contract FundMe{
     using PriceConverter for uint256;
 
-    uint256 minimumUSD = 5e18;
+    uint256 public constant minimumUSD = 5e18;
+
+    address public immutable owner;
 
     address[] public funders;
     mapping(address funder => uint256 amountInUSD) public addressToAmountinUSD;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         require( msg.value.getConversionRate() >= minimumUSD , "Not enough funds!");
@@ -20,7 +28,8 @@ contract FundMe{
         addressToAmountinUSD[msg.sender] = addressToAmountinUSD[msg.sender] + msg.value;
     }
 
-    function withdraw() public{
+    function withdraw() public onlyOwner{
+        
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++)
         {
             address funder = funders[funderIndex];
@@ -30,7 +39,7 @@ contract FundMe{
 
         //transfer
         // payable(msg.sender).transfer(address(this).balance);
-        
+
         // send
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess,"Send failed!");
@@ -40,4 +49,20 @@ contract FundMe{
         require(send, "Call Failed!");
     }
 
+    modifier onlyOwner(){
+        // require(msg.sender == owner, "Must be owner!");
+        if(msg.sender != owner)
+        {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable{
+        fund();
+    }
 }
